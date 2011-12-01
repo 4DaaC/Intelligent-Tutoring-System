592022e3bfb3cab40f358bab1ccfc3333102f4ae
Joel <joel@main.(none)>
2011-12-01 16:32:49 -0500

    First commit for server code

--- server/app.js
+++ server/app.js
@@ -4,8 +4,21 @@
  */
 
 var express = require('express');
+var fs = require('fs');
+var mysql = require('mysql');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt')
+});
+
+var client = mysql.createClient({
+  user: 'softeng11',
+  host: 'localhost',
+  password: 'softengdb11'
+});
+
+client.query('USE softeng11');
 
 // Configuration
 
@@ -30,6 +43,29 @@ app.configure('production', function(){
 });
 
 // Routes
+app.post('/student', function(req, res) {
+  var user = req.body.username;
+  console.log(req.body.username);
+  client.query("INSERT INTO Users (username, auth_level) VALUES ('"+user+"', '8')",
+    function(err, result, fields) {
+      console.log("HERE");
+      if(err) {
+        console.log(err);
+        res.json({});
+      } else {
+	console.log(result);
+	res.json({id: result.insertId});
+      }
+  });
+});
+
+app.post('/admin', function(req, res) {
+
+});
+
+app.post('/prof', function(req, res) {
+
+});
 
 app.get('/', function(req, res){
   res.render('index', {
