cc8bfe549ac97cb4d271622664a1b5e9c1969002
Joel Dominic <jdominic@rucs.radford.edu>
2011-12-05 20:31:26 -0500

    Get Profs AJAX call works

--- server/app.js
+++ server/app.js
@@ -76,14 +76,64 @@ app.post('*',requireLogin);
 
 // Routes
 app.get('/mobile/profs', function(req, res) {
-	var user = crypt.decrypt(req.query.user);
-	client.query("SELECT username, uid "+
-				 "FROM Users NATURAL JOIN Classes "+
-				 "WHERE cid IN (SELECT cid "+ 
-				 				"FROM (Class_List a NATURAL JOIN Users b) "+
-				 				"WHERE username = '"+user+"')", function(err, results, fields) {
-		res.send(results);
-	});
+	if(req.query.user == undefined) { 
+		client.query("SELECT username, uid "+
+				 "FROM Users "+
+				 "WHERE auth_level = 1", function(err, results, fields) {
+					console.log(results);
+					res.send(results);				
+				});
+	} else {
+		var user = crypt.decrypt(req.query.user);
+		client.query("SELECT username, uid "+
+					 "FROM Users NATURAL JOIN Classes "+
+					 "WHERE cid IN (SELECT cid "+ 
+					 				"FROM (Class_List a NATURAL JOIN Users b) "+
+				 					"WHERE username = '"+user+"')", function(err, results, fields) {
+			console.log(results);
+			res.send(results);
+		});
+	}
+});
+
+app.get('/mobile/classes', function(req, res) {
+	var prof = req.query.prof;
+	var user = req.query.user;
+	if(prof == undefined && user == undefined) {
+		client.query("SELECT name, cid, classlimit "+
+					 "FROM Classes", function(err, results, fields) {
+						console.log(results);
+						res.send(results);
+					 });
+	} else if(user == undefined) {
+		client.query("SELECT name, cid, classlimit "+
+				 	 "FROM Classes "+
+				 	 "WHERE uid IN (SELECT uid "+
+				 			  	   "FROM Users "+
+							  	   "WHERE username = '"+prof+"')", function(err, results, fields) {
+						console.log(results);						  
+						res.send(results);
+					});
+	} else if(prof == undefined) {
+		client.query("SELECT name, cid, classlimit "+
+				 	 "FROM Classes "+
+					 "WHERE cid IN (SELECT cid "+
+					 			   "FROM Class_List NATURAL JOIN Users "+
+								   "WHERE username = '"+user+"')", function(err, results, fields) {
+						console.log(results);
+						res.send(results);
+					});
+	} else {
+		client.query("SELECT name, cid, classlimit "+
+					 "FROM Classes "+
+					 "WHERE cid IN (SELECT cid "+
+					 			   "FROM Class_List NATURAL JOIN Users "+
+					 			   "WHERE username = '"+user+"') AND uid IN (SELECT uid FROM Users WHERE username = '"+prof+"')"
+				 	 , function(err, results, fields) {
+			console.log(results);		
+			res.send(results);
+		});
+	}
 });
 
 app.post('/admin', function(req, res) {
@@ -122,5 +172,5 @@ app.get('/logout',function(req,res){
  req.session.destroy();
  res.redirect('/');
 });
-app.listen(3000);
+app.listen(3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
