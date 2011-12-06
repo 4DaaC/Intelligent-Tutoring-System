c9c0fe5c07af39119c3fef46e3e4e080b0c24066
Matt Rohr <mrohr@nearinfinity.com>
2011-12-06 00:19:28 -0500

    mobile app work, download and view professors/classes/quizzes

--- www/js/its.js
+++ www/js/its.js
@@ -1,11 +1,14 @@
+var cname = 'its-login-username'; 
+var baseUrl = "http://rucs.radford.edu:3000";
 getUsername = function(){
   var storage = window.localStorage;
-  return storage.getItem("ITS-Username");
+  return storage.getItem(cname);
 }
 
 setUsername = function(username){
   var storage = window.localStorage;
-  storage.setItem("ITS-Username",username);
+  storage.setItem(cname,username);
+
 }
 
 loggedIn = function(){
@@ -13,13 +16,86 @@ loggedIn = function(){
 }
 
 logoutUser = function(){
-  window.localStorage.removeItem('ITS-Username');
+  window.localStorage.removeItem(cname);
 }
 
 goBack = function(){
   history.go(-1);
 }
 
-getClasses = function(username){
-  return new Array();
+setProfs = function(profObject){
+  var storage = window.localStorage;
+  storage.setItem(getUsername() + ":Professors",JSON.stringify(profObject));
 }
+getProfs = function(){
+  var storage = window.localStorage;
+  return JSON.parse(storage.getItem(getUsername() + ":Professors"));
+}
+downloadProfs = function(callback){
+  var url = baseUrl + "/mobile/profs?user=" + escape(encrypt(getUsername()));
+  console.log("GET:" + url);
+  $.ajax({
+    url:url,
+    success:function(data){
+      setProfs(data);
+      callback(data);
+    },
+    error:function(){
+      callback(null,"Problem Downloading Professors");
+    }
+  });
+}
+
+setClasses = function(profName,classesObject){
+  var storage = window.localStorage;
+  storage.setItem(getUsername() + ":" + profName + ":Classes",JSON.stringify(classesObject));
+}
+
+getClasses = function(profName){
+  var storage = window.localStorage;
+  return JSON.parse(storage.getItem(getUsername() + ":" + profName + ":Classes"));
+}
+
+downloadClasses = function(profName,callback){
+  var url = baseUrl + "/mobile/classes?prof=" + escape(encrypt(profName)) + "&user=" + escape(encrypt(getUsername()));
+  console.log("GET:" + url);
+  $.ajax({
+    url:url,
+    success:function(data){
+      setClasses(profName,data);
+      callback(data);
+    },
+    error:function(){
+      callback(null,"Problem Downloading Classes");
+    }
+  });
+  
+}
+
+setQuizzes = function(cid,quizzesObject){
+  var storage = window.localStorage;
+  storage.setItem(getUsername() + ":" + cid + ":Quizzes",JSON.stringify(quizzesObject));
+}
+
+getQuizzes = function(cid){
+  var storage = window.localStorage;
+  return JSON.parse(storage.getItem(getUsername() + ":" + cid + ":Quizzes"));
+}
+
+downloadQuizzes = function(cid,callback){
+  var url = baseUrl + "/mobile/quizzes?cid=" + escape(cid) + "&user=" + escape(encrypt(getUsername()));
+  console.log("GET:" + url);
+  $.ajax({
+    url:url,
+    success:function(data){
+      setQuizzes(cid,data);
+      callback(data);
+    },
+    error:function(){
+      callback(null,"Problem Downloading Quizzes");
+    }
+  });
+  
+}
+
+
