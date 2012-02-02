b9ba7b38663e86f3168f2f844936da9b82b7c037
Matt Rohr <mjrohr@radford.edu>
2012-02-02 13:32:29 -0500

    added update to app.js file

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@
 var express = require('express');
 var fs = require('fs');
 var mysql = require('mysql');
-var crypt = require('./crypt.js');
 var app = module.exports = express.createServer();
 var config = require('./config')
 /*var app = module.exports = express.createServer({
@@ -78,93 +77,6 @@ var requireLogin = function(req,res,next){
   }
 }
 
-app.get('*',requireLogin);
-app.put('*',requireLogin);
-app.post('*',requireLogin);
-
-// Routes
-app.get('/mobile/profs', function(req, res) {
-	if(req.query.user == undefined) { 
-		client.query("SELECT username, uid "+
-				 "FROM Users "+
-				 "WHERE auth_level = 1", function(err, results, fields) {
-					console.log(results);
-					res.send(results);				
-				});
-	} else {
-		var user = crypt.decrypt(req.query.user);
-		client.query("SELECT username, uid "+
-					 "FROM Users NATURAL JOIN Classes "+
-					 "WHERE cid IN (SELECT cid "+ 
-					 				"FROM (Class_List a NATURAL JOIN Users b) "+
-				 					"WHERE username = ?)",[user], function(err, results, fields) {
-			console.log(results);
-			res.send(results);
-		});
-	}
-});
-
-app.get('/mobile/quizzes', function(req, res) {
-  var cid = req.query.cid;
-  var user = req.query.user;
-  if(user == undefined || cid == undefined){
-    req.send(500);
-  }else{
-    user = crypt.decrypt(user).toLowerCase();
-    var qString = "SELECT qid,Quizzes.name FROM Quizzes,Classes WHERE Quizzes.cid = Classes.cid AND Quizzes.cid = '" + cid + "' AND " + 
-      "Quizzes.cid IN (SELECT cid FROM (Class_List a NATURAL JOIN Users b) WHERE username = ?)";
-    console.log(qString);
-    client.query(qString,[user], function(err,results,fields){
-        console.log(results);
-        res.send(results);
-      });
-  }
-});
-
-app.get('/mobile/classes', function(req, res) {
-	var prof = req.query.prof;
-	var user = req.query.user;
-  if(user != undefined){
-    user = crypt.decrypt(user);
-  }
-  if(prof != undefined){
-    prof = crypt.decrypt(prof);
-  }
-	if(prof == undefined && user == undefined) {
-		client.query("SELECT name, cid, classlimit FROM Classes", function(err, results, fields) {
-						console.log(results);
-						res.send(results);
-					 });
-	} else if(user == undefined) {
-		client.query("SELECT name, cid, classlimit "+
-				 	 "FROM Classes "+
-				 	 "WHERE uid IN (SELECT uid "+
-				 			  	   "FROM Users "+
-							  	   "WHERE username = ?)", [prof],function(err, results, fields) {
-						console.log(results);						  
-						res.send(results);
-					});
-	} else if(prof == undefined) {
-		client.query("SELECT name, cid, classlimit "+
-				 	 "FROM Classes "+
-					 "WHERE cid IN (SELECT cid "+
-					 			   "FROM Class_List NATURAL JOIN Users "+
-								   "WHERE username = ?)",[user], function(err, results, fields) {
-						console.log(results);
-						res.send(results);
-					});
-	} else {
-		client.query("SELECT name, cid, classlimit "+
-					 "FROM Classes "+
-					 "WHERE cid IN (SELECT cid "+
-					 			   "FROM Class_List NATURAL JOIN Users "+
-					 			   "WHERE username = ?) AND uid IN (SELECT uid FROM Users WHERE username = ?)"
-				 	 , [user,prof],function(err, results, fields) {
-			console.log(results);		
-			res.send(results);
-		});
-	}
-});
 var authCheck = function(req,callback){
   var qString = "SELECT auth_level FROM Users WHERE username = ?";
 	var q = client.query(qString,[req.session.user.username],function(err,results,fields) {
@@ -175,6 +87,13 @@ var authCheck = function(req,callback){
     }
   });
 }
+// Routes
+
+app.get('*',requireLogin);
+app.put('*',requireLogin);
+app.post('*',requireLogin);
+require('./routes/index')(app,client);
+
 
 app.get('/admin', function(req, res) {
   authCheck(req,function(auth_level){  
