c9b3cdcbf18b468a3ddc5bef800191248240822d
Matt Rohr <mjrohr@radford.edu>
2012-02-13 16:04:50 -0500

    mobile route tweaks

--- server/routes/mobile.js
+++ server/routes/mobile.js
@@ -5,47 +5,71 @@ app.get('/mobile/profs', function(req, res) {
 		client.query("SELECT username, uid "+
 				 "FROM Users "+
 				 "WHERE auth_level = 1", function(err, results, fields) {
-					console.log(results);
-					res.send(results);				
+					if(err){
+            console.log(err);
+            res.send(500);
+          }else{
+            console.log(results);
+				  	res.send(results);				
+          }
 				});
 	} else {
 		var user = crypt.decrypt(req.query.user);
-		client.query("SELECT username, uid "+
+		var sqlString = "SELECT username, uid "+
 					 "FROM Users NATURAL JOIN Classes "+
 					 "WHERE cid IN (SELECT cid "+ 
 					 				"FROM (Class_List a NATURAL JOIN Users b) "+
-				 					"WHERE username = ?)",[user], function(err, results, fields) {
-			console.log(results);
-			res.send(results);
+				 					"WHERE username = ?)";
+    console.log('user: ' + user);
+    client.query(sqlString,[user], function(err, results, fields) {
+			if(err){
+            console.log(err);
+            res.send(500);
+          }else{
+            console.log(results);
+				  	res.send(results);				
+          }
 		});
 	}
 });
 
 app.get('/mobile/quiz', function(req,res){
   var qid = req.query.qid;
-  if(qid == undefined){
+  var user = req.query.user;
+  if(qid == undefined || user == undefined){
+    console.log("undefined");
     res.send(500);
   }else{
-    var qString = "SELECT * FROM Questions WHERE Questions.qid = ?";
-    client.query(qString,[qid],function(err,results){
+    var user = crypt.decrypt(user);
+    console.log("User = " + user);
+    client.query("SELECT * FROM Questions WHERE qid = ?", [qid], function(err,questions){
       if(err){
         console.log(err);
         res.send(500);
       }else{
-        console.log(results);
-        res.send(results);
+        var sqlString = "SELECT Answers.questid, Answers.saved_answer FROM Answers,Questions,Users WHERE " +
+           "Users.username = ? AND Questions.qid = ? AND Users.uid = Answers.uid AND Answers.questid = Questions.questid";
+        client.query(sqlString,[user,qid],function(errors,answers){
+          if(errors){
+            console.log(errors);
+            res.send(500);
+          }else{
+            res.send({questions:questions, answers:answers});
+          }
+        });
       }
     });
   }
 });
+app.get('/mobile/test', function(req,res){res.send('hello world')});
 app.get('/mobile/quizzes', function(req, res) {
   var cid = req.query.cid;
   var user = req.query.user;
   if(user == undefined || cid == undefined){
-    req.send(500);
+    res.send(500);
   }else{
     user = crypt.decrypt(user).toLowerCase();
-    var qString = "SELECT qid,Quizzes.name FROM Quizzes,Classes WHERE Quizzes.cid = Classes.cid AND Quizzes.cid = '" + cid + "' AND " + 
+    var qString = "SELECT Quizzes.* FROM Quizzes,Classes WHERE Quizzes.cid = Classes.cid AND Quizzes.cid = '" + cid + "' AND " + 
       "Quizzes.cid IN (SELECT cid FROM (Class_List a NATURAL JOIN Users b) WHERE username = ?)";
     console.log(qString);
     client.query(qString,[user], function(err,results,fields){
