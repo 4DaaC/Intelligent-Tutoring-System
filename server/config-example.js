55e00de050b2157c25c24b1c2f1f10a33a591f2f
Matt Rohr <mjrohr@radford.edu>
2012-03-20 10:17:36 -0400

    added example config file

---
+++ server/config-example.js
@@ -0,0 +1,10 @@
+//rename this file to config.js to use
+var config ={};
+config.db = {};
+config.port = 3000;
+config.requireLogin = true;
+config.db.user = 'quiz';
+config.db.host = 'localhost';
+config.db.password = 'EaYY95dN724MaE5P';
+config.db.database = 'quiz';
+module.exports = config;
