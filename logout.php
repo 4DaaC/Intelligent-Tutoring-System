27710da97d66bb4c9d96cb8627a811b10b37a5ff
Matt Rohr <mrohr@nearinfinity.com>
2012-05-07 13:42:59 -0400

    removed unused files

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cname = "its-login-username";
-setcookie($cname,"",time() - 3600,"/", ".radford.edu",1);
-?>
-  <form action="https://php.radford.edu/~it-auth/vsp09/login.php" method="post">
-    <input type="hidden" name="url" value="https://php.radford.edu/~mjrohr/its/login.php">
-    <input type="hidden" name="cname" value="<?php echo $cname;?>">
-  </form>
-  <script>document.forms[0].submit();</script>
